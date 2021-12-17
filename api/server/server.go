187375fde3e5c11bd15705f1dda73dd823b1c04b
Stephen Salt <kntajus@hotmail.com>
2021-12-17 23:09:27 +0000

    Add callable endpoint to retrieve item

--- api/server/server.go
+++ api/server/server.go
@@ -2,17 +2,24 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/kntajus/sampleapp/api/port"
 	"github.com/kntajus/sampleapp/protos"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
 	updatePortsURL = "/update"
+	getPortURLStem = "/ports/"
 )
+
+var getPortURLFormat = regexp.MustCompile("^/ports/[A-Z]{5}$")
 
 type Server struct {
 	portClient protos.PortDomainServiceClient
@@ -28,6 +35,7 @@ func New(portClient protos.PortDomainServiceClient) *Server {
 
 func (s *Server) Serve() error {
 	http.HandleFunc(updatePortsURL, s.updatePorts)
+	http.HandleFunc(getPortURLStem, s.getPort)
 	return s.server.ListenAndServe()
 }
 
@@ -58,4 +66,30 @@ func (s *Server) updatePorts(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+func (s *Server) getPort(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	if !getPortURLFormat.MatchString(path) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	portID := path[len(getPortURLStem):]
+
+	resp, err := s.portClient.GetPort(context.Background(), &protos.GetPortRequest{Id: portID})
+	if err != nil {
+		httpCode := http.StatusInternalServerError
+		st := status.Convert(err)
+		if st.Code() == codes.NotFound {
+			httpCode = http.StatusNotFound
+		}
+		w.WriteHeader(httpCode)
+		return
+	}
+
+	json.NewEncoder(w).Encode(resp.GetPort())
 }
